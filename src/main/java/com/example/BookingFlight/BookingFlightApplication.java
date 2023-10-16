55e94f03308cbe6419c27972a59797d43f9af83e
proushothkoushal <proushothkoushal@gmail.com>
2023-10-16 11:12:37 +0530

    files

---
+++ src/main/java/com/example/BookingFlight/BookingFlightApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookingFlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingFlightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookingFlight/BookingFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookingFlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
